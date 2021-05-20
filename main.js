f86d420527a4c829283a3d788fdd5875734f82b9
Adrian_Mendez <admendez89@gmail.com>
2021-05-20 20:54:37 -0300

    actualiacion

---
+++ main.js
@@ -0,0 +1,20 @@
+console.clear();
+function typingEffect(element,speed){
+  let text=element.innerHTML;
+  element.innerHTML="";
+ var i=0;
+  var timer=setInterval(function(){
+    if(i<text.length){
+      element.append(text.charAt(i))
+      i++;
+    }else{
+      clearInterval(timer);
+    }
+  },speed)
+  
+}
+
+
+
+const p=document.querySelector('p');
+typingEffect(p,150);
